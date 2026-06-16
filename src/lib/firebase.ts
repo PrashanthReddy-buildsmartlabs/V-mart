96cb4d2421166c9ce6a57831a7d2337f36f8b8f6
PrashanthReddy-buildsmartlabs <buildsmart.labs@gmail.com>
2026-06-16 18:36:35 +0530

    nothing much

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9jXwJDGz6IXey8Ml32y8nQyAhOKNrRC8",
-  authDomain: "buildsmartlabs-c3083.firebaseapp.com",
-  projectId: "buildsmartlabs-c3083",
-  storageBucket: "buildsmartlabs-c3083.firebasestorage.app",
-  messagingSenderId: "346626995578",
-  appId: "1:346626995578:web:179c66ad745402107f92c3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
